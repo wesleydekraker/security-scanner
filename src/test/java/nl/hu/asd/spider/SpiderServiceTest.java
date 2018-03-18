da3baee2d5ad0110e77054a51e58a8baa092e4e1
Wesley de Kraker <github@wesleydekraker.com>
2018-03-18 23:49:59 +0100

    Removed unnecessary import

--- src/test/java/nl/hu/asd/spider/SpiderServiceTest.java
+++ src/test/java/nl/hu/asd/spider/SpiderServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import java.net.MalformedURLException;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
