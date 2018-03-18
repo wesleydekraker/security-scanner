a7b6b12c3872e3e3cdcf7b7ae64c48ba023d60dc
Wesley de Kraker <github@wesleydekraker.com>
2018-03-18 21:59:47 +0100

    Added assert to spider test

--- src/test/java/nl/hu/asd/spider/SpiderTest.java
+++ src/test/java/nl/hu/asd/spider/SpiderTest.java
@@ -5,16 +5,18 @@ import org.junit.Test;
 
 import java.net.MalformedURLException;
 
+import static org.junit.Assert.assertEquals;
+
 public class SpiderTest
 {
     @Test
-    public void testConstructor() throws MalformedURLException {
+    public void testStart() throws MalformedURLException {
         Spider spider = new Spider();
         spider.setHttpClient(new HttpClientStub());
         spider.setMaxChildren(4);
         spider.setMaxDepth(5);
 
         spider.start("http://example.com/index.html");
-        System.out.println(spider.getVisitedUrls());
+        assertEquals(10, spider.getVisitedUrls().size());
     }
 }
