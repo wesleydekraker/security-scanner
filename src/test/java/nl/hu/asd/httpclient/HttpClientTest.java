e9540f50db0e9257e0a5aa6c7e074ec18ddf49ae
Wesley de Kraker <github@wesleydekraker.com>
2018-03-18 18:25:26 +0100

    Prevented test class from connecting to the internet

--- src/test/java/nl/hu/asd/httpclient/HttpClientTest.java
+++ src/test/java/nl/hu/asd/httpclient/HttpClientTest.java
@@ -1,16 +1,16 @@
-package nl.hu.asd.httpclient;
-
-import org.junit.Test;
-
-public class HttpClientTest
-{
-    @Test
-    public void testGet() throws HttpClientException {
-        HttpClient httpClient = new HttpClient();
-
-        SimpleHttpRes response = httpClient.connect("http://example.com/");
-
-        System.out.println(response.getStatusCode());
-        System.out.println(response.getBody());
-    }
-}
+//package nl.hu.asd.httpclient;
+//
+//import org.junit.Test;
+//
+//public class HttpClientTest
+//{
+//    @Test
+//    public void testGet() throws HttpClientException {
+//        HttpClient httpClient = new HttpClient();
+//
+//        SimpleHttpRes response = httpClient.connect("http://example.com/");
+//
+//        System.out.println(response.getStatusCode());
+//        System.out.println(response.getBody());
+//    }
+//}
