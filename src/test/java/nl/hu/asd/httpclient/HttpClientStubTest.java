990e447c507e51eb5e81697d3672caab8d81342e
Wesley de Kraker <github@wesleydekraker.com>
2018-03-18 23:25:09 +0100

    Added assert to stub test

--- src/test/java/nl/hu/asd/httpclient/HttpClientStubTest.java
+++ src/test/java/nl/hu/asd/httpclient/HttpClientStubTest.java
@@ -2,14 +2,17 @@ package nl.hu.asd.httpclient;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class HttpClientStubTest
 {
     @Test
     public void testGet() throws HttpClientException {
         HttpClientStub httpClientStub = new HttpClientStub();
 
-        SimpleHttpRes response = httpClientStub.connect("http://example.com/tokyo/index.html");
-        System.out.println(response.getStatusCode());
-        System.out.println(response.getBody());
+        SimpleHttpRes response = httpClientStub.connect("http://example.com/tokyo/map.html");
+        assertEquals(200, response.getStatusCode());
+        assertTrue(response.getBody().contains("Map of Tokyo"));
     }
 }
