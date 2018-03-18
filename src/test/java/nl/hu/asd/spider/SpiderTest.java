6ab57e450126d6b304279f8927fcbcd81f28d50a
Wesley de Kraker <github@wesleydekraker.com>
2018-03-18 22:25:58 +0100

    Added additional tests

--- src/test/java/nl/hu/asd/spider/SpiderTest.java
+++ src/test/java/nl/hu/asd/spider/SpiderTest.java
@@ -13,10 +13,28 @@ public class SpiderTest
     public void testStart() throws MalformedURLException {
         Spider spider = new Spider();
         spider.setHttpClient(new HttpClientStub());
-        spider.setMaxChildren(4);
-        spider.setMaxDepth(5);
 
         spider.start("http://example.com/index.html");
         assertEquals(10, spider.getVisitedUrls().size());
+    }
+
+    @Test
+    public void testStartMaxDepth() throws MalformedURLException {
+        Spider spider = new Spider();
+        spider.setHttpClient(new HttpClientStub());
+        spider.setMaxDepth(4);
+
+        spider.start("http://example.com/index.html");
+        assertEquals(9, spider.getVisitedUrls().size());
+    }
+
+    @Test
+    public void testStartMaxChildren() throws MalformedURLException {
+        Spider spider = new Spider();
+        spider.setHttpClient(new HttpClientStub());
+        spider.setMaxChildren(1);
+
+        spider.start("http://example.com/index.html");
+        assertEquals(6, spider.getVisitedUrls().size());
     }
 }
