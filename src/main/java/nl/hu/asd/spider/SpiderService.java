ad0417a30a123d0353b9d9f30bb6fbe8c64a80c3
Wesley de Kraker <github@wesleydekraker.com>
2018-03-18 18:46:36 +0100

    Created spider service

---
+++ src/main/java/nl/hu/asd/spider/SpiderService.java
@@ -0,0 +1,19 @@
+package nl.hu.asd.spider;
+
+import nl.hu.asd.httpclient.HttpClient;
+
+import java.net.MalformedURLException;
+import java.util.Set;
+
+public class SpiderService {
+    public static Set<SimpleUrl> getUrls(String startUrl, int maxChildren, int maxDepth) throws MalformedURLException {
+        Spider spider = new Spider();
+        spider.setHttpClient(new HttpClient());
+        spider.setMaxChildren(maxChildren);
+        spider.setMaxDepth(maxDepth);
+
+        spider.start(startUrl);
+        return spider.getVisitedUrls();
+    }
+
+}
